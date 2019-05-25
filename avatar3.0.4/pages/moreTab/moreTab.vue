53e47adac54dd9b17f2a0c97ca85092edf898198
yqking <yqking0320@gmail.com>
2019-05-25 11:03:30 +0800

    H5平台适配HX199

---
+++ avatar3.0.4/pages/moreTab/moreTab.vue
@@ -0,0 +1,22 @@
+<template>
+	<view>
+		多余tab页面
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
